c59e9cdf3e7cf747402ede29d92244ac68cb2c8a
Onesoft <onesoftqwq@163.com>
2024-11-18 19:53:27 +0800

    更改版本号

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import shutil
 import threading
 from tkinter import filedialog
 
-version = [1,0]
+version = [1,1]
 
 os.system(f'title XTCEasyRootPlus v{version[0]}.{version[1]}')
 console = Console()
